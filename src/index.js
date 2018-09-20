fdeaf6f975a96d0979b52b345a710b52fbf23c01
Vitaly Dmitriev <vitalik.dmitriev1995@gmail.com>
2018-09-20 23:31:49 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.replace(/\s/g, "");
+  function checkСoincidence(){    
+    for(var i=0; i<bracketsConfig.length; i++){
+      var reg = bracketsConfig[i][0]+bracketsConfig[i][1];
+      var found = str.includes(reg);
+      if(found !== false){
+        deleteСoincidence(reg);
+      }
+    }
+    
+  }
+  function deleteСoincidence(reg){
+    str = str.replace(reg, "");
+    checkСoincidence();
+  }
+  
+  checkСoincidence();
+  if(str == 0){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
